e04fc9eb4b902ef2ba28d48d7cafc6254fe041e2
1Kuznetsov <i.kuznetsov5@g.nsu.ru>
2023-11-21 11:36:08 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+import ru_local as ru
+# 1 2 3 4 5 9 10
+# 6 7 8 10 11
+
+
+if __name__ == '__main__':
+    turtle.tracer(0)
+    action = int(input(ru.CHOICE))
+
+    while action != 0:
+        pass
+
+    turtle.update()
+    turtle.done()
+
