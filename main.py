24fce377707839301edd93fbd73792d17ef6c541
yamako6321 <124650861+yamako6321@users.noreply.github.com>
2023-11-26 11:30:03 +0700

    a

--- main.py
+++ main.py
@@ -1,6 +1,88 @@
 import turtle
 import ru_local as ru
 
+# def square(order, size):
+#     if order == 0:
+#         return turtle.fd(size)
+#
+#
+# def binary_tree(order, size):
+#     if  order == 0:
+#         return turtle.fd(size)
+
+
+def fractal_branch(order, size):
+    if order == 0:
+        turtle.left(180)
+        return
+
+    x = size / (order + 1)
+
+    turtle.pensize(3)
+    turtle.speed(300)
+
+
+    for i in range(order):
+        turtle.forward(x)
+        turtle.left(45)
+        fractal_branch(order - i - 1, 0.5 * x * (order - i - 1))
+        turtle.left(90)
+        fractal_branch(order - i - 1, 0.5 * x * (order - i - 1))
+        turtle.right(135)
+
+    turtle.forward(x)
+    turtle.left(180)
+    turtle.forward(size)
+
+def fractal_branch_2(order, size):
+    turtle.up()
+    turtle.left(90)
+    turtle.down()
+    fractal_branch(order, size)
+
+
+def Koch_curve(order, size):
+    if order == 0:
+        return turtle.fd(size)
+    Koch_curve(order - 1, size / 3)
+    turtle.lt(60)
+    Koch_curve(order - 1, size / 3)
+    turtle.rt(120)
+    Koch_curve(order - 1, size / 3)
+    turtle.lt(60)
+    Koch_curve(order - 1, size / 3)
+
+def snowflake_Koha(order, size):
+    for i in range(3):
+        Koch_curve(order, size)
+        turtle.right(120)
+
+
+def levi_curve(order, size):
+    if order == 0:
+        return turtle.forward(size)
+    turtle.speed(400)
+    turtle.left(45)
+    levi_curve(order - 1, size / 2)
+    turtle.right(90)
+    levi_curve(order - 1, size / 2)
+    turtle.left(45)
+
+# def Mashas_new_quarter_square(order, size):
+#     for i in range(6):
+#         turtle.fd(size)
+#         turtle.rt(90)
+# def Mashas_new_quarter(order, size):
+#     if size <= 1:
+#         return None
+#
+#     turtle.speed(400)
+#     turtle.pensize(2)
+#
+#     Mashas_new_quarter_square(order, size)
+#     turtle.lt(5)
+#     return Mashas_new_quarter(order, 0.9 * size)
+
 
 def minkowski(order, size):
     if order == 0:
@@ -83,6 +165,15 @@ if __name__ == '__main__':
     deep = int(input(ru.ORDER))
     length = float(input(ru.SIZE))
 
+    if action == 3:
+        fractal_branch_2(deep, length)
+
+    if action == 4:
+        Koch_curve(deep, length)
+
+    if action == 5:
+        snowflake_Koha(deep, length)
+
     if action == 6:
         turtle.up()
         turtle.goto(-700, 0)
@@ -100,6 +191,9 @@ if __name__ == '__main__':
         turtle.goto(-700, 0)
         turtle.down()
         ice_fractal_2(deep, length)
+
+    if action == 9:
+        levi_curve(deep, length)
 
     elif action == 11:
         turtle.up()
